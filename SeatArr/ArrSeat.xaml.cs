a8a6d4536955355d855c140db426eeb54b814019
trailblazer7 <junseo1453@gmail.com>
2022-03-31 00:53:11 +0900

    초기 커밋.

---
+++ SeatArr/ArrSeat.xaml.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SeatArr
+{
+    /// <summary>
+    /// ArrSeat.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class ArrSeat : Window
+    {
+        private List<List<int>> ClassSeats;
+        private List<Label> ClassMates;
+        private int row;
+        private int column;
+        private int StudentsNumber;
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Application.Current.Shutdown();
+        }
+
+        public ArrSeat(string ClassInput)
+        {
+            InitializeComponent();
+            ClassSeats = new List<List<int>>();
+            ClassMates = new List<Label>();
+            StudentsNumber = Sum(ClassInput);
+            InitStudents();
+            InitTable(ClassInput);
+            PrintOnScreen();
+        }
+
+        public ArrSeat(string ClassInput, List<string> custom_class)
+        {
+            InitializeComponent();
+            ClassSeats = new List<List<int>>();
+            ClassMates = new List<Label>();
+            StudentsNumber = Sum(ClassInput);
+            InitStudents(custom_class);
+            InitTable(ClassInput);
+            PrintOnScreen();
+        }
+
+        int MAX(List<int> list)
+        {
+            int M = list[0];
+            foreach(int i in list)
+            {
+                M = M > i ? M : i;
+            }
+            return M;
+        }
+
+        int Sum(string str)
+        {
+            int temp=0;
+            foreach(char i in str)
+            {
+                temp += i-48;
+            }
+            return temp;
+        }
+
+        private void InitStudents()
+        {
+            for(int i = 0; i < StudentsNumber; i++)
+            {
+                Label student = new Label();
+                student.Content = i+1;
+                student.FontSize = 30;
+                student.Background = new SolidColorBrush(Color.FromRgb(237, 197, 85));
+                ClassMates.Add(student);
+                ClassRoom.Children.Add(student);
+            }
+        }
+
+        private void InitStudents(List<string> c_s)
+        {
+            for (int i = 0; i < StudentsNumber; i++)
+            {
+                Label student = new Label();
+                student.Content = c_s[i];
+                student.FontSize = 30;
+                student.Background = new SolidColorBrush(Color.FromRgb(237, 197, 85));
+                ClassMates.Add(student);
+                ClassRoom.Children.Add(student);
+            }
+        }
+
+        private void InitTable(string classinput)
+        {
+            List<int> SeatSetting = new List<int>();
+
+            /*this part make temporary list needed for making table*/
+            for (int i = 0; i < classinput.Length; i++)
+            {
+                SeatSetting.Add(classinput[i] - 48);
+            }
+
+            /*this part makes seats*/
+            int student_no = 0;
+
+            row = SeatSetting.Count;
+            column = MAX(SeatSetting);
+
+            for (int i = 0; i < row; i++)
+            {
+                List<int> temp = new List<int>();
+                for (int j = 0; j < column; j++)
+                {
+                    if (j < SeatSetting[i])
+                    {
+                        temp.Add(student_no);
+                        student_no++;
+                    }
+                    else
+                    {
+                        temp.Add(-1);
+                    }
+                }
+                ClassSeats.Add(temp);
+            }
+
+            /*this part sets up the grid*/
+            for (int i = 0; i < column; i++)
+            {
+                ClassRoom.RowDefinitions.Add(new RowDefinition());
+            }
+            for (int i = 0; i < row; i++)
+            {
+                ClassRoom.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+        }
+
+        /*private void PrintSeat()
+        {
+            Console.WriteLine(GetSeatSheet());
+        }*/
+
+        private void PrintOnScreen()
+        {
+            for (int i=0; i<column; i++)
+            {
+                for(int j=0; j < row; j++)
+                {
+                    int student_number = ClassSeats[j][i];
+                    if (student_number == -1)
+                    {
+                        continue;
+                    }
+                    Label class_mate = ClassMates[student_number];
+                    Grid.SetRow(class_mate, i);
+                    Grid.SetColumn(class_mate, j);
+                }
+            }
+        }
+
+
+        private void MixSeat()
+        {
+            ClassMates = ClassMates.OrderBy(a => Guid.NewGuid()).ToList();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.S && Keyboard.Modifiers == ModifierKeys.Control)
+            {
+                MessageBox.Show("CTRL + S");
+                return;
+            }
+            if (e.Key != Key.Enter && e.Key != Key.Space)
+            {
+                return;
+            }
+            MixSeat();
+            PrintOnScreen();
+        }
+
+        /*private int SaveFile()
+        {
+            HwpActionClass hwpActionClass = new HwpActionClass();
+            hwpActionClass.Execute("");
+        }*/
+    }
+}
---
+++ SeatArr/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SeatArr
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                ProcessClassInformation();
+            }
+        }
+
+        int Sum(string str)
+        {
+            int temp = 0;
+            foreach (char i in str)
+            {
+                temp += i - 48;
+            }
+            return temp;
+        }
+
+        private void ProcessClassInformation()
+        {
+            if (IsValidClass())
+            {
+                if (CheckBox_CustomStudent.IsChecked == true)
+                {
+                    List<string> custom_class = GetCustomClass();
+                    if (custom_class == null | Sum(ClassInput.Text) != custom_class.Count)
+                    {
+                        MessageBox.Show("'반, 이름 저장하기' 파일이 손상되었거나\n파일에 적혀있는 학생수와 입력한 학생수가 불일치합니다.", "quite fatal error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        return;
+                    }
+                    this.Hide();
+                    ArrSeat arrSeat = new ArrSeat(ClassInput.Text, custom_class);
+                    arrSeat.ShowDialog();
+                }
+                else
+                {
+                    this.Hide();
+                    ArrSeat arrSeat = new ArrSeat(ClassInput.Text);
+                    arrSeat.ShowDialog();
+                }
+            }
+            else
+            {
+                MessageBox.Show( "잘못된 입력값입니다! 다시 시도해주세요" , "not fatal error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+        
+        private bool IsValidClass()
+        {
+            if (ClassInput.Text == "")
+            {
+                return false;
+            }
+
+            foreach (Char i in ClassInput.Text)
+            {
+                if (!Char.IsDigit(i))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private List<string> GetCustomClass()
+        {
+            StreamReader sr = new StreamReader("반, 이름 지정하기.txt");
+            string temp="";
+            while (sr.Peek() >= 0) { 
+                temp = sr.ReadLine();
+            }
+            sr.Close();
+            List<string> custrom_class = new List<string>(temp.Split(':')[1].Split(','));
+            return custrom_class;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ProcessClassInformation();
+        }
+    }
+}
